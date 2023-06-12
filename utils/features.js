d236783d1ed549436d6da319f173c73f1a4b17b2
Tejas Chauhan <tdchauhan07@gmail.com>
2023-06-13 02:08:27 +0530

    backend deployed

--- utils/features.js
+++ utils/features.js
@@ -2,7 +2,9 @@ import jwt from 'jsonwebtoken';
 export const sendCookie = (res,user,message) => {
     const token = jwt.sign({_id:user._id},process.env.JWTTOKEN)
     res.status(201).cookie("token",token,{
-        httpOnly:true
+        httpOnly:true,
+        sameSite:"none",
+        secure:true
     }).json({
         success:true,
         message:message
