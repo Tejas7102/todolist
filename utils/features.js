ee60b48a118687887758c5db595dd662737195bf
Tejas Chauhan <tdchauhan07@gmail.com>
2023-06-13 02:27:16 +0530

    resolving the cookie error

--- utils/features.js
+++ utils/features.js
@@ -3,8 +3,8 @@ export const sendCookie = (res,user,message) => {
     const token = jwt.sign({_id:user._id},process.env.JWTTOKEN)
     res.status(201).cookie("token",token,{
         httpOnly:true,
-        sameSite:"none",
-        secure:true
+        sameSite:process.env.NODE_ENV==="Development"?"lax":"none",
+        secure:process.env.NODE_ENV==="Development"?false:true
     }).json({
         success:true,
         message:message
