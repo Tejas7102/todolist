1c1e4dc1f5fb81caa82b1cd824f7aa862bb4d940
Tejas Chauhan <tdchauhan07@gmail.com>
2023-06-13 02:11:16 +0530

    backend deployed

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import taskRouter from './routes/task.js'
 import { config } from 'dotenv'
 import cookieParser from 'cookie-parser'
 import { errorMiddleware } from './middleware/error.js'
+//server doesn't allow us that we can use two diffrent url for frontend and backend
+//so we have to install cors to resolve that issue
+import cors from 'cors'
 export const app = express()
 config({
     path:"./data/config.env"
@@ -14,6 +17,11 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 //to access cookie
 app.use(cookieParser())
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
 //to use router
 app.use("/api/v1/user",useRouter)
 app.use("/api/v1/task",taskRouter)
